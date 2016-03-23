30d4af7ff0d8b5f81ff2613ad5bbb65bb4014b41
Jure Valjavec <valjavecjur@gmail.com>
2016-03-23 11:18:30 +0100

    Test commitanja

--- gpio.h
+++ gpio.h
@@ -4,5 +4,5 @@ BOOL set_pin_out (UINT8 port, UINT8 pin);
 BOOL set_pin_in (UINT8 port, UINT8 pin);
 BOOL toggle_pin (UINT8 port, UINT8 pin);
 BOOL set_pin_state (UINT8 port, UINT8 pin, UINT8 state);
-
+/*test git commit*/
 /*coment test*/
