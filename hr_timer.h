cbcaa8e50b80a9016aa752a3091d61566d5a42f4
Jure Valjavec <valjavecjur@gmail.com>
2016-04-01 13:10:04 +0200

    C:\Users\Jure\AppData\Local\Temp\Sccs5

--- hr_timer.h
+++ hr_timer.h
@@ -3,8 +3,17 @@
 
 #include "types.h"
 
+/************************* data structures ******************/
 
+struct heart_rate
+{
+	UINT16 pulse;
+	UINT16 timer_new_val;
+	UINT16 timer_old_val;
+}; 
+struct heart_rate hr_data;
 
+/************************************************************/
 
 void tc0_init (void);
 void start_hr_timer (void);
